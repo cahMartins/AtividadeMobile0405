d3531f3fbecc774f81b5b964acc80b295f7ac026
origin <https://github.com/cahMartins/AtividadeMobile0405.git>
2022-05-04 19:32:10 -0300

    Atividade de mobile 04/05/2002

---
+++ android/app/src/main/kotlin/com/yourcompany/flutter_app_statefull_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.yourcompany.flutter_app_statefull_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "StateLess",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String nome = "";
+  void _showcontent() {
+    showDialog(
+      context: context, barrierDismissible: false, // user must tap button!
+
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Olá $nome'),
+          actions: [
+            TextButton(
+              child: Text('Ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Estudando Statefull"),
+            centerTitle: true,
+          ),
+          body: Center(
+              child: Container(
+                width: 300,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(border: OutlineInputBorder(), labelText: "Insira o seu nome:"),
+                      onChanged: (String str) {
+                        setState(() {
+                          nome = str;
+                        });
+                      },
+                    ),
+                    TextButton(
+                      onPressed: _showcontent,
+                      style: TextButton.styleFrom(
+                        primary: Colors.white,
+                        backgroundColor: Colors.blue,
+                      ),
+                      child: Text('Abrir'),
+                    ),
+                  ],
+                ),
+              )),
+        ));
+  }
+}
